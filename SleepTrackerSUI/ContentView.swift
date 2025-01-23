d93201d8cab5c27cf7350c2308a3ab6c04ad3382
Kirill Khomicevich <83289514+KirillHomy@users.noreply.github.com>
2025-01-23 18:07:28 +0200

    Initial Commit

---
+++ SleepTrackerSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SleepTrackerSUI
+//
+//  Created by Kirill Khomicevich on 23.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
